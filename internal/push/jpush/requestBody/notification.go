c027df9b60c4f00628f41eb7ad5e9f876fa90e92
edram <edram@qq.com>
2023-03-04 08:04:56 +0000

    feat: support jpush title

--- internal/push/jpush/requestBody/notification.go
+++ internal/push/jpush/requestBody/notification.go
@@ -12,13 +12,17 @@ type Notification struct {
 
 type Android struct {
 	Alert  string `json:"alert,omitempty"`
+	Title  string `json:"title,omitempty"`
 	Intent struct {
 		URL string `json:"url,omitempty"`
 	} `json:"intent,omitempty"`
 	Extras Extras `json:"extras"`
 }
 type Ios struct {
-	Alert          string `json:"alert,omitempty"`
+	Alert struct {
+		Title string `json:"title,omitempty"`
+		Body  string `json:"body,omitempty"`
+	} `json:"alert,omitempty"`
 	Sound          string `json:"sound,omitempty"`
 	Badge          string `json:"badge,omitempty"`
 	Extras         Extras `json:"extras"`
@@ -29,12 +33,14 @@ type Extras struct {
 	ClientMsgID string `json:"clientMsgID"`
 }
 
-func (n *Notification) SetAlert(alert string) {
+func (n *Notification) SetAlert(title string, alert string) {
 	n.Alert = alert
 	n.Android.Alert = alert
+	n.Android.Title = title
 	n.SetAndroidIntent()
-	n.IOS.Alert = alert
-	n.IOS.Sound = "default"
+	n.IOS.Alert.Body = alert
+	n.IOS.Alert.Title = title
+
 	n.IOS.Badge = "+1"
 }
 
